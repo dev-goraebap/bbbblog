ccb7f2094872db78ecb1ae327f177153eb9cc9b6
dev-goraebap <dev.goraebap@gmail.com>
2025-06-16 22:40:22 +0900

    feat: 업로드버튼 초기화 기능 추가

--- app/javascript/controllers/upload_box/upload_box_controller.js
+++ app/javascript/controllers/upload_box/upload_box_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 
 export class UploadBoxController extends Controller {
-  static targets = ["fileInput", "previewArea", "previewItemTemplate"];
+  static targets = [
+    "fileInput",
+    "previewArea",
+    "previewItemTemplate",
+    "clearButtonTemplate",
+  ];
 
   connect() {
     // if (this.isMultipleValue)
@@ -9,7 +14,7 @@ export class UploadBoxController extends Controller {
   }
 
   onChange(e) {
-    this.previewAreaTarget.innerHTML = '';
+    this.previewAreaTarget.innerHTML = "";
 
     const files = Array.from(e.target.files);
 
@@ -25,20 +30,37 @@ export class UploadBoxController extends Controller {
     fileMetadata.forEach((x) => {
       this.#renderPreview(x);
     });
+
+    const clearButtonEl =
+      this.clearButtonTemplateTarget.content.cloneNode(true);
+    this.previewAreaTarget.appendChild(clearButtonEl);
+  }
+
+  onClear() {
+    this.fileInputTarget.value = '';
+    this.previewAreaTarget.innerHTML = '';
   }
 
   #renderPreview(metadata) {
-    const { url, name, size } = metadata;
+    const { url, name, size, type } = metadata;
     const previewItemEl =
       this.previewItemTemplateTarget.content.cloneNode(true);
 
     const imgEl = previewItemEl.querySelector("[x-data='img']");
     const nameEl = previewItemEl.querySelector("[x-data='name']");
     const sizeEl = previewItemEl.querySelector("[x-data='size']");
+    const typeEl = previewItemEl.querySelector("[x-data='type']");
     imgEl.src = url;
     nameEl.textContent = name;
-    sizeEl.textContent = size;
+    sizeEl.textContent = this.#formatFileSize(size);
+    typeEl.textContent = type;
 
     this.previewAreaTarget.appendChild(previewItemEl);
+  }
+
+  #formatFileSize(bytes) {
+    if (bytes < 1024) return bytes + " B";
+    else if (bytes < 1048576) return (bytes / 1024).toFixed(1) + " KB";
+    else return (bytes / 1048576).toFixed(1) + " MB";
   }
 }
