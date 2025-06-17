445c36efc56511a89a0ef029db85042a8622c229
dev-goraebap <dev.goraebap@gmail.com>
2025-06-17 20:05:37 +0900

    style: create 메서드에서 디버깅 로그 제거

--- app/controllers/lab/image_uploads_controller.rb
+++ app/controllers/lab/image_uploads_controller.rb
@@ -11,12 +11,6 @@ class Lab::ImageUploadsController < ApplicationController
   end
 
   def create
-    puts "==================="
-    puts test_object_params
-    puts test_object_params[:images]
-    puts test_object_params[:name]
-    puts "==================="
-
     @test_object = TestObject.new(test_object_params)
     if @test_object.save
       flash[:notice] = "이미지 업로드 성공! 파일 개수: #{@test_object.images.count}"
