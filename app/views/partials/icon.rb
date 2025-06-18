4e72b6b121840ee897515e3b72b84db7a250ed64
dev-goraebap <dev.goraebap@gmail.com>
2025-06-19 03:37:13 +0900

    feat: 아이콘 뷰 템플릿에 이미지 아이콘 추가 및 SVG 코드 리팩토링

--- app/views/partials/icon.rb
+++ app/views/partials/icon.rb
@@ -7,7 +7,7 @@ class Views::Partials::Icon < Phlex::HTML
   def view_template
     # icon_name과 일치하는 메서드가 있는지 확인
     if respond_to?(:"icon_#{@name}", true)
-      div(class: @classes) do
+      div(class: "size-6 #{@classes}") do
         # 동적으로 해당 아이콘 메서드 호출
         send(:"icon_#{@name}")
       end
@@ -24,7 +24,7 @@ class Views::Partials::Icon < Phlex::HTML
   # 각 아이콘에 대한 메서드 정의
   def icon_close
     raw(<<~SVG.html_safe)
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
       </svg>
     SVG
@@ -32,7 +32,7 @@ class Views::Partials::Icon < Phlex::HTML
 
   def icon_check
     raw(<<~SVG.html_safe)
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
       </svg>
     SVG
@@ -40,8 +40,16 @@ class Views::Partials::Icon < Phlex::HTML
 
   def icon_warning
     raw(<<~SVG.html_safe)
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+      </svg>
+    SVG
+  end
+
+  def icon_image
+    raw(<<~SVG.html_safe)
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
       </svg>
     SVG
   end
