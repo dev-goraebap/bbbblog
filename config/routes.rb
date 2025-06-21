27fc24bcecf78badb842132cd20be47ac680f37f
dev-goraebap <dev.goraebap@gmail.com>
2025-06-21 19:45:28 +0900

    feat: 관리자 포스트 스케폴드 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :posts
+  end
   root "home#index"
 
   namespace :lab, module: "lab" do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_17_094416) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_21_102824) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,22 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_17_094416) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "status", default: "draft"
+    t.datetime "published_at"
+    t.integer "views_count", default: 0
+    t.string "slug"
+    t.string "description"
+    t.integer "reading_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["published_at"], name: "index_posts_on_published_at"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
+    t.index ["status"], name: "index_posts_on_status"
   end
 
   create_table "test_objects", force: :cascade do |t|
